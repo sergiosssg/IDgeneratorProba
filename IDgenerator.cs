78fe43d5ea39aafbbde8b91c8446387331da747c
sergiosssg <sergiosssg@gmail.com>
2021-06-29 16:05:52 +0300

    добавил класс public class IDgenerator? использующий библиотеку https://github.com/MatthewKing/DeviceId :      умолчательным конструктором,   с приватныой переменной :      =- private string _deviceId   с функцией :      =- public string getIDOnFluent().

---
+++ IDgenerator.cs
@@ -0,0 +1,24 @@
+﻿using DeviceId;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDgeneratorProba
+{
+    public class IDgenerator
+    {
+        private string _deviceId;
+
+        public IDgenerator()
+        {
+            _deviceId = new DeviceIdBuilder().AddMacAddress().AddProcessorId().AddMotherboardSerialNumber().ToString();
+        }
+
+        public string getIDOnFluent()
+        {
+            return _deviceId;
+        }
+    }
+}
