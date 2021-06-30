c02fd5a5568474a1eba3b9951ece9af843ea4ab3
sergiosssg <sergiosssg@gmail.com>
2021-06-30 14:27:19 +0300

    изменил конструктор класса  IDgenerator :     добавил получение       MAC адреса, как строки,       ID процессора, как строки,       Номера материской платы, как строки.

--- IDgenerator.cs
+++ IDgenerator.cs
@@ -17,16 +17,22 @@ namespace IDgeneratorProba
             long iRes;
 
             int iMAChash;
+            string sMACadr;
+            string sMotherBoardNumber;
+            string sProcessorID;
             int iProcessorID;
             int iMotherBoardNumber;
             DeviceIdBuilder dIDb = new DeviceIdBuilder();
             _deviceId = dIDb.AddMacAddress().AddProcessorId().AddMotherboardSerialNumber().ToString();
+            sMACadr = dIDb.AddMacAddress().ToString();
             iMAChash = dIDb.AddMacAddress().GetHashCode();
             DeviceIdBuilder MACAddr = dIDb.AddMacAddress();
             iProcessorID = dIDb.AddProcessorId().GetHashCode();
+            sProcessorID = dIDb.AddProcessorId().ToString();
             DeviceIdBuilder ProcessorID = dIDb.AddProcessorId();
             iMotherBoardNumber = dIDb.AddMotherboardSerialNumber().GetHashCode();
             DeviceIdBuilder MotheBoardNumber = dIDb.AddMotherboardSerialNumber();
+            sMotherBoardNumber = dIDb.AddMotherboardSerialNumber().ToString();
             iRes = 0xFF << 8;
         }
 
